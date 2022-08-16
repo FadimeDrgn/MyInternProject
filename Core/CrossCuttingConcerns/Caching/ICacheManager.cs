bf2d635bf914348a2a5f889744ee509575be0f13
Fadime Durğun <durgunfadime@gmail.com>
2022-08-16 14:12:14 +0300

    Refactoring

---
+++ Core/CrossCuttingConcerns/Caching/ICacheManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Caching
+{
+    public interface ICacheManager
+    {
+        //kullanıcı hangisini kullanmak isterse farklı alternatifler sunduk burada
+        T Get<T>(string key);
+        object Get(string key);
+        void Add(string key, object value, int duration);
+        //verinin cache olup olmadığını kontrol eder
+        bool IsAdd(string key);
+        //bir veriyi cache'den kaldırmak için
+        void Remove(string key);
+        //parametli olanlarda pattern kullanırız mesela burada deriz ki isminde kategori olanları kaldır
+        void RemoveByPattern(string pattern);
+    }
+}
--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,6 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,7 +15,10 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection serviceCollection)
         {
+            //IMemoryCache _memoryCache; implementasyoun karşılığı, arka planda hazır bir ICacheManager instance oluşturuyor
+            serviceCollection.AddMemoryCache();
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
         }
     }
 }
